dfbc86e9f64404db94c31d84934c29739f193908
aa122002-cyber <aa122002@gs.gsc.aoyama.ac.jp>
2025-11-21 14:58:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
   center: [141.148667,42.497528], // 中心座標
-  zoom: 16, // ズームレベル
+  zoom: 3, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
